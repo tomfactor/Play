9067e800d63fb8ea4392712880862f6f93bad797
tomfactor <130264524+tomfactor@users.noreply.github.com>
2023-05-07 00:53:29 +0300

    Update Connectfour.php

--- src/Entity/Game/Connectfour.php
+++ src/Entity/Game/Connectfour.php
@@ -42,7 +42,7 @@ class Connectfour extends Game
      *
      * @var string
      */
-    protected static $description = 'Connect Four is a connection game in which the players take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid.';
+    protected static $description = 'Connect Four هي لعبة اتصال يتناوب فيها اللاعبون على إسقاط الأقراص الملونة من الأعلى إلى شبكة معلقة رأسيًا مكونة من سبعة أعمدة وستة صفوف';
 
     /**
      * Game thumbnail image
